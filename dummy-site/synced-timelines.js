b0f1c5e0d9de9e000f5217fb0cbf2ad0c839f9e6
Alexander Hofbauer <alexander.hofbauer@bildungsserver.wien>
2024-01-16 17:44:43 +0100

    feat: implement timeline syncing using vis-timeline methods

--- dummy-site/synced-timelines.js
+++ dummy-site/synced-timelines.js
@@ -30,7 +30,7 @@ Number.prototype.clamp = function(min, max) {
   */
 const $timelines = document.querySelectorAll(`[${_timelineIdAttr}]`);
 const $syncedTimelines = document.querySelectorAll(`[${_timelineIdAttr}]:not([${_timelineSyncedWithAttr}=""])`);
-const visTimelines = []
+const visTimelines = {}
 
 /**
  * Figure out whether events overlap and put them into different lanes.
@@ -44,35 +44,15 @@ $timelines.forEach(function($timeline) {
 
   const $container = $timeline.querySelector('.vis-timeline')
 
-
-  const today = new Date()
-  today.setHours(0, 0, 0, 0)
-  const tomorrow = new Date(today)
-  tomorrow.setDate(today.getDate() + 1)
-
-  let minimumDate = Date.now()
-  let maximumDate = Date.now()
-
   const data = new vis.DataSet([
     ...Array.from($events).map(function($event, idx) {
-      const item = {
+      return {
         id: idx,
         start: $event.dataset.startdate,
         end: $event.dataset.enddate,
         content: $event,
         active: false
       }
-
-      let eventStartdate = new Date($event.dataset.startdate)
-      let eventEnddate = new Date($event.dataset.enddate)
-      if (eventStartdate < minimumDate) {
-        minimumDate = eventStartdate
-      }
-      if (eventEnddate > maximumDate) {
-        maximumDate = eventEnddate
-      }
-
-      return item
     }),
   ])
 
@@ -90,8 +70,7 @@ $timelines.forEach(function($timeline) {
       scale: 'year',
       step: 1
     },
-    //min: minimumDate,
-    //max: maximumDate,
+    zoomMin: 315360000000000 / 1000 / 2, // 5 years
     showCurrentTime: false,
     template: function(item, element, data) {
       const $dateLine = data.content.querySelector('.event-line.time');
@@ -110,6 +89,7 @@ $timelines.forEach(function($timeline) {
   }
 
   const timeline = new vis.Timeline($container, data, options);
+  visTimelines[$timeline.dataset.timelineId] = timeline
   //console.log(timeline)
 
   const $background = $timeline.querySelector('.vis-panel.vis-background')
@@ -140,85 +120,29 @@ $timelines.forEach(function($timeline) {
    */
   timeline.on('changed', handleActiveStates)
 
+  timeline.on('rangechange', handleTimelineSync)
+
+  /**
+   * TODO: Add comments to this file!
+   */
+  function handleTimelineSync(event) {
+    /**
+     * The idea of this pattern was to return a new handler for each timeline that does not run the
+     * `timeline.setWindow()` method on the timeline that is currently being actively scrolled.
+     */
+    const $eventTarget = event.event?.target !== undefined ? event.event.target.closest('.timeline-viewport') : undefined
+    const targetTimelineIds = $timeline.dataset.timelineSyncedWith.split(',')
+
+    targetTimelineIds.forEach(function(targetTimelineId) {
+      if ($eventTarget !== undefined && targetTimelineId !== $eventTarget.dataset.timelineId) {
+        visTimelines[targetTimelineId].setWindow(event.start, event.end, { animation: { duration: 200, easingFunction: 'easeInOutQuad' } })
+      }
+    })
+  }
+
   /**
    * Hide original track with JS, so it looks nice even if JS is disabled
    */
   const $track = $timeline.querySelector(_trackSelector)
   $track.style.display = 'none'; // hide original track
 })
-
-/**
-  * Add "scroll" EventListeners to all synced timelines
-  *
-  * We are not using an arrow function here to open a new scope for every timeline.
-  */
-$syncedTimelines.forEach(function($timeline) {
-  /**
-    * Get scrollable part of the timeline (we call this part "track")
-    */
-  const $track = $timeline.querySelector(_trackSelector)
-  /**
-    * Get widths of timeline container and track to calculate ratios when scrolling
-    */
-  // width of timeline "viewport"
-  $timeline.dataset.timelineWidth = $timeline.clientWidth;
-  // width of the whole scrollable element
-  $timeline.dataset.trackWidth = $track.scrollWidth;
-  // number of pixels the $track can be scrolled around
-  $timeline.dataset.scrollableWidth = ($timeline.dataset.trackWidth - $timeline.dataset.timelineWidth).min(0);
-
-  /**
-   * selectors for all timeilnes this one is synced with
-   */
-  const syncedWidthSelectors = convertSyncedWithToSelectors($timeline.dataset.timelineSyncedWith)
-  /**
-* Get all synced timelines from the DOM to run scrollTo() on them
-*/
-  const $syncedWith = document.querySelectorAll(syncedWidthSelectors)
-
-  /**
-    * Add the actual EventListeners to the track elements
-    *
-    * TODO: debounce this using setTimeout() or getAnimationFrame() or something (s. https://developer.mozilla.org/en-US/docs/Web/API/Element/scroll_event)
-    */
-  $track.addEventListener('scroll', () => {
-    /**
-      * get scroll ratio of scrolled guidelines
-      */
-    const scrolledRatio = ($track.scrollLeft / $timeline.dataset.scrollableWidth).clamp(0, 1);
-
-    /**
-      * For each synced timeline scroll to the same position relative to the scrollable width
-      */
-    $syncedWith.forEach(function($syncedTimeline) {
-      /**
-        * calculate same ratio for synced guidelines
-        */
-      const scrollTo = $syncedTimeline.dataset.scrollableWidth * scrolledRatio
-
-      /**
-        * scroll the synced guidelines to this ratio in pixels
-        */
-      const $syncedTrack = $syncedTimeline.querySelector(_trackSelector)
-      /**
-       * FIX: for some reason this does not work on right-most handful of pixels
-       *      probably due to some clamping issues or padding/margin/offset inaccuracies
-       */
-      if ($syncedTimeline.dataset.timelineId !== $timeline.dataset.timelineId) {
-        $syncedTrack.scrollTo({ left: scrollTo })
-      }
-    })
-  });
-})
-
-/**
- * Functions section
- */
-function convertSyncedWithToSelectors(syncedWith) {
-  return syncedWith
-    .split(',') // split the comma-separated list into an array (TODO: this might not be necessary if we use group ids to sync multiple timelines)
-    .map(function(timelineId) { // create valid selectors from the ids
-      return `[${_timelineIdAttr}="${timelineId}"]`
-    })
-    .join(',') // create a valid CSS selector string from the selectors array
-}
